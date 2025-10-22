dbe465f12265c411950f4f785687730289807918
anamika0707 <141743181+anamika0707@users.noreply.github.com>
2025-10-22 16:12:36 +0530

    landing page completed

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,42 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { Button } from "./ui/button"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Avatar } from "./ui/avatar";
+import { LinkIcon, LogOut } from "lucide-react";
 
 const Header = () => {
-  return <nav>
-    <Link>
-    <img  src="./logo.png" className="h-16" alt="Trimrr logo"/></Link>
 
-    <div></div>
+  const navigate = useNavigate();
+  const user = true;
+  return <nav className="py-4 flex justify-between items-center">
+    <Link to="/">
+      <img src="./logo.png" className="h-16" alt="Trimrr logo" /></Link>
+
+    <div>
+      {!user ? (
+        <Button onClick={() => navigate("/auth")}>Login</Button>
+
+      ) : (
+        <DropdownMenu>
+          <DropdownMenuTrigger  className="w-10 rounded-full overflow-hidden"><Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar></DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Anamika</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <LinkIcon className="mr-2 h-4 w-4"/>
+              My Links</DropdownMenuItem>
+            <DropdownMenuItem className="text-red-400"><LogOut className="mr-2 h-4 w-4"/><span>Logout</span> </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+
+      }
+
+    </div>
   </nav>
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 // Always enable dark mode globally
-document.documentElement.classList.add('dark');
+// document.documentElement.classList.add('dark');
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
